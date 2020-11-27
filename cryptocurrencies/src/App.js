50ba9ae552188d8a87dd52ecb2f182be9cd6c67e
Sanket Parshuram Patil <53385614+sanket9006@users.noreply.github.com>
2020-11-28 03:20:45 +0530

    Added

---
+++ cryptocurrencies/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import FetchRandomUser from "./components/FetchRandomUser";
+
+class App extends Component {
+  state = {
+    visible: true
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <FetchRandomUser />
+      </div>
+    );
+  }
+}
+
+export default App;
