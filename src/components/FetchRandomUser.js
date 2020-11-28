b8a5a1acdb7596f00601815270d4f2ecc891a77c
Sanket Parshuram Patil <53385614+sanket9006@users.noreply.github.com>
2020-11-28 14:36:14 +0530

    added

--- src/components/FetchRandomUser.js
+++ src/components/FetchRandomUser.js
@@ -8,9 +8,19 @@ export default class FetchRandomUser extends React.Component {
 
   async componentDidMount() {
     const url = "https://api.randomuser.me/";
+    const url1 = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=INR/";
+
     const response = await fetch(url);
+    const response1 = await fetch(url1);
+
     const data = await response.json();
+    const data1 = await response1.json();
+
+    console.debug(data);
+
     this.setState({ person: data.results[0], loading: false });
+    this.setState({ cryptocurrency: data1.BTC, loading: false });
+
   }
 
   render() {
@@ -24,6 +34,7 @@ export default class FetchRandomUser extends React.Component {
 
     return (
       <div>
+       
         <div>{this.state.person.name.title}</div>
         <div>{this.state.person.name.first}</div>
         <div>{this.state.person.name.last}</div>
